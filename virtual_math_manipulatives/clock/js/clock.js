c7309bd77f2577c4290b767709eda1425a4b43cf
Patrick Coxall <mr.coxall@mths.ca>
2021-05-28 21:27:08 +0000

    improved clock stub, with html parameters being passed in

--- virtual_math_manipulatives/clock/js/clock.js
+++ virtual_math_manipulatives/clock/js/clock.js
@@ -1,25 +1,58 @@
+/* global Phaser */
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
-// This file contains the JS functions for clock.html
-
-"use strict"
+// This is the Phaser3 scene in a single file
 
 const params = new URLSearchParams(document.location.search);
 const hours = params.get('h');
 const minutes = params.get('m');
 
-console.log(hours)
-console.log(minutes)
+const gameScene = new Phaser.Scene('Game');
 
-function clockTime () {
-  // this function prints out the clock's time
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  scene: {
+    init,
+    preload,
+    create,
+    update,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  // set background color
+  backgroundColor: 0xffffff,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    //autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
+const phaserScene = new Phaser.Game(config)
 
-  // input
 
-  // process
+function init () {
+  console.log(hours)
 
-  // output
-  //document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' mm³.'
+}
+function preload () {
+  console.log('Clock Scene')
+}
+
+function create (data) {
+  console.log(minutes)
+}
+
+function update (time, delta) {
+  if (time < 3000) {
+    console.log('Counting')
+  }
 }
---
+++ virtual_math_manipulatives/clock/js/clock2.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for clock.html
+
+"use strict"
+
+const params = new URLSearchParams(document.location.search);
+const hours = params.get('h');
+const minutes = params.get('m');
+
+console.log(hours)
+console.log(minutes)
+
+function clockTime () {
+  // this function prints out the clock's time
+
+  // input
+
+  // process
+
+  // output
+  //document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' mm³.'
+}
